b0c3ecbf050897e99d0794220e25d2a1078cc5f5
giovifav <wip.gio@gmail.com>
2022-08-02 21:30:21 +0200

    map selection and lean

--- main.lua
+++ main.lua
@@ -21,6 +21,27 @@ end
 
 function love.update(dt)
     terrain:update(dt)
+    
+
+    if love.keyboard.wasPressed("1")then
+        terrain = require('src.terrain')("maps/C1W.png","maps/C1D.png")
+
+    end
+    if love.keyboard.wasPressed("2")then
+        terrain = require('src.terrain')("maps/C2W.png","maps/C2D.png")
+
+    end
+    if love.keyboard.wasPressed("3")then
+        terrain = require('src.terrain')("maps/C3W.png","maps/C3D.png")
+
+    end
+    if love.keyboard.wasPressed("4")then
+        terrain = require('src.terrain')("maps/C4W.png","maps/C4D.png")
+
+    end
+
+
+
     love.keyboard.keysPressed = {}
 end
 
